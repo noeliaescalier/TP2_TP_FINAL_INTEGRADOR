caa2a66ca8b7cd288bc8f7b0ad3dd9e67e22d8ec
noeliaescalier <noesc@frba.utn.edu.ar>
2025-10-30 22:00:58 -0300

    subo estructura de proyecto

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import express from "express"
+import router  from "./routes/route.js"
+
+const app = express()
+const PORT = 8080
+
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+
+app.use("/api",router)
+app.listen(PORT,()=> console.log("Server Running..."))
